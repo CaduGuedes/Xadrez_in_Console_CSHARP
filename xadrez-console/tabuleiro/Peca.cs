c57320a91874b897d1a155ad44b13cccea9c6acf
Cadu Guedes <caduguedesmg@gmail.com>
2019-07-10 10:21:52 -0300

    Mudando nomde do metodo para movimentoPossivel

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -35,7 +35,7 @@ namespace tabuleiro {
             return false;
         }
 
-        public bool podeMorverPara(Posicao pos) {
+        public bool movimentoPossivel(Posicao pos) {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
 
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -79,7 +79,7 @@ namespace xadrez {
         }
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
-            if (!tab.peca(origem).podeMorverPara(destino)) {
+            if (!tab.peca(origem).movimentoPossivel(destino)) {
                 throw new TabuleiroException("Posicção de Destino inválida!");
             }
         }
