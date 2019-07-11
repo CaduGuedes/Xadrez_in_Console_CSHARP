788a3fae7933d2e3f66288df260b0df750b5eeee
Cadu Guedes <caduguedesmg@gmail.com>
2019-07-11 11:23:22 -0300

    Jogada Especial Promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -119,6 +119,20 @@ namespace xadrez {
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            //#JogadaEspecial Promocao
+            if(p is Peao) {
+                if (p.cor == Cor.Branca && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             }
@@ -134,7 +148,6 @@ namespace xadrez {
             }
 
             //#JogadaEspecial En Passant
-            Peca p = tab.peca(destino);
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)) {
                 vulneravelEnPassant = p;
             }
